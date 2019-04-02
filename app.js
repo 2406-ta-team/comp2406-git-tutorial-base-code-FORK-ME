bf5883d2c16d544987389d80141ba6469512eeeb
Matthew <matthew.e.brown.17@gmail.com>
2019-04-02 03:25:23 -0400

    Tidy up code
    - Replace string concatenation with template literals
    - Remove multi-lines in ternary operators
    - Surround ternary operators' Boolean components with brackets to
      improve readability
    - Shorten single-line anonymous functions; no {} required
    - Shorten single-line if-statements; no {} required

--- app.js
+++ app.js
@@ -23,15 +23,13 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
+app.use((req, res, next) => next(createError(404)));
 
 // error handler
 app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = (req.app.get('env') === 'development') ? err : {};
 
   // render the error page
   res.status(err.status || 500);
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'COMP 2406' });
-});
+router.get('/', (req, res, next) => res.render('index', { title: 'COMP 2406' }));
 
 module.exports = router;
